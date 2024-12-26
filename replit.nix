d97d5c9b7ccb0b1e17c569aff55a2c2e67f3a879
Your Name <your@email.com>
2024-12-25 16:22:06 -0800

    update packages

--- replit.nix
+++ replit.nix
@@ -4,5 +4,9 @@
     pkgs.python39Packages.pip
     pkgs.python39Packages.setuptools
     pkgs.python39Packages.wheel
+    pkgs.python39Packages.tweepy
+    pkgs.python39Packages.pydantic
+    pkgs.python39Packages.requests
+    pkgs.python39Packages.aiohttp
   ];
 }
